37477861ca5e46b9bd29ab38a7063d973220105f
Suhit Kumar <suhit.kmr1@gmail.com>
2018-06-30 11:26:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,4 +17,6 @@ function sayHiToGrandma(string){
     {return ('I can\'t hear you!')}
   else if(string===uppercase)
     {return ('YES INDEED!')}
+  else if(string==='I love you, Grandma')
+   {return ('I love you, too.')}
 }
